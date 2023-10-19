29a63292f13e19e9ae2b4de132a848e5e8b4cd93
Abe Omaits <aomaits@gmail.com>
2023-10-19 14:16:54 -0500

    set up models and schemas folders and files

---
+++ server/models/Characters.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// 
+const characterSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nickname: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+    height: {
+        type: String,
+    },
+    weight: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    alignment: {
+        type: String,
+        required: true,
+    },
+    items: {
+        type: Schema.Types.ObjectId,
+        ref: 'Item',
+    }
+});
+
+// Create a character model using the characterSchema
+const Character = mongoose.model('Character', characterSchema);
+
+// Export the Character model 
+module.exports = Character;
