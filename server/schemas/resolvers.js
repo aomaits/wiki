83ce7f25c75ac64b69a8a7632c64a765c1e7d143
Abe Omaits <aomaits@gmail.com>
2023-10-25 13:29:50 -0500

    updated resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,39 +1,19 @@
 const { AuthenticationError } = require('apollo-server-express');
-const {  } = require('../models');
+const { Characters, Items, Locations } = require('../models');
 
 const resolvers = {
     Query: {
-        // villager: async (parent, { _id }) => {
-        //     return await Villager.findById(_id).populate('requests').populate('village');
-        // },
-        // villagers: async () => {
-        //     return await Villager.find();
-        // },
-        // village: async (parent, { _id }) => {
-        //     return await Village.findById(_id).populate('villagers').populate('admin');
-        // },
-        // villages: async () => {
-        //     return await Village.find().populate('villagers').populate('admin');
-        // },
-        // requests: async () => {
-        //     return await Request.find().populate('authorId').populate({
-        //         path: 'response',
-        //         populate: 'claimId'
-        //     });
-        // },
-        // request: async (parent, { _id }) => {
-        //     return await Request.findById(_id).populate('authorId').populate({
-        //         path: 'response',
-        //         populate: 'claimId'
-        //     });
-        // },
-        // comment: async (parent, { _id }) => {
-        //     return await Comment.findById(_id).populate('authorId').populate('requestId');
-        // },
-        // comments: async () => {
-        //     return await Comment.find().populate('authorId').populate('requestId');
-        // }
+        characters: async () => {
+            return await Characters.find();
+        },
+        items: async () => {
+            return await Items.find().populate('characterId');
+        },
+        locations: async () => {
+            return await Locations.find();
+        },
     },
+    
     Mutation: {
     },
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,33 @@ const { gql } = require('apollo-server-express');
 
 // GraphQL schemas, queries, and mutations
 const typeDefs = gql`
+    type Character {
+        _id: ID
+        name: String
+        nickname: String
+        race: String
+        gender: String
+        age: Int
+        height: String
+        weight: String
+        description: String
+        alignment: String
+        items: [Items]
+    }
+
+    type Item {
+        _id: ID
+        name: String
+        description: String
+        characterId: [Characters]
+    }
+
+    type Location {
+        _id: ID
+        name: String
+        description: String
+        type: String
+    }
 `;
 
 module.exports = typeDefs;
