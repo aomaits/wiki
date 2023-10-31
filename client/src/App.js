b085ea849d5f9a575374b4b3bf1d814b591f0890
Abe Omaits <aomaits@gmail.com>
2023-10-31 14:57:59 -0500

    working on the App.js and Card component to try and pull necessary info from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import React, { useState, useEffect } from "react";
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Search from "./components/Search/Search";
 import Card from "./components/Card/Card";
@@ -8,21 +15,34 @@ import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 
+
+
 function App() {
+
+  const client = new ApolloClient({
+    uri: '/graphql', // Replace with your GraphQL API endpoint
+    cache: new InMemoryCache(),
+  });
+
+  loadErrorMessages();
+
+
   return (
-<div className="App">
-  <h1 className="text-center mb-3">Characters</h1>
-  <div className="container">
-  <div className="row">
-    Filter component will be placed here
-    <div className="col-lg-8 col-12">
-      <div className="row">
-        Card component will be placed here
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1 className="text-center mb-3">Characters</h1>
+        <div className="container">
+          <div className="row">
+            Filter component will be placed here
+            <div className="col-lg-8 col-12">
+              <div className="row">
+                Card component will be placed here
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  </div>
-</div>
+    </ApolloProvider>
   );
 }
 
